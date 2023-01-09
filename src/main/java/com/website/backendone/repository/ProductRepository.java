37a8234aa75a6a09aeb99e1e8907b860b19b5d06
unknown <amsubhadeep@gmail.com>
2023-01-09 20:25:00 +0530

    Added repositories

---
+++ src/main/java/com/website/backendone/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.website.backendone.repository;
+
+import com.website.backendone.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
---
+++ src/main/java/com/website/backendone/repository/SectionRepository.java
@@ -0,0 +1,9 @@
+package com.website.backendone.repository;
+
+import com.website.backendone.entity.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+}
