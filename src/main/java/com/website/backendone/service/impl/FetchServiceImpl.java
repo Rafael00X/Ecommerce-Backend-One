211a92d4ae09962329ed1f0ab0b92a57c890c1d6
unknown <amsubhadeep@gmail.com>
2023-02-06 21:43:01 +0530

    Moved fetch url to application.properties

--- src/main/java/com/website/backendone/service/impl/FetchServiceImpl.java
+++ src/main/java/com/website/backendone/service/impl/FetchServiceImpl.java
@@ -3,14 +3,15 @@ package com.website.backendone.service.impl;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.website.backendone.model.User;
 import com.website.backendone.service.FetchService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FetchServiceImpl implements FetchService {
-
-    private static final String URL = "http://localhost:8082";
+    @Value("${backend-two.api.url}")
+    private String url;
 
     @Override
     public User validateToken(String token) {
@@ -20,7 +21,7 @@ public class FetchServiceImpl implements FetchService {
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<User> response = restTemplate.exchange(URL + "/validate", HttpMethod.GET, requestEntity, User.class, 0);
+        ResponseEntity<User> response = restTemplate.exchange(url + "/validate", HttpMethod.GET, requestEntity, User.class, 0);
         if (!response.getStatusCode().is2xxSuccessful())
             throw new RuntimeException("Invalid jwt");
         return response.getBody();
