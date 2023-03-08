0caa5e283572cae542c7dfb7bd8396c5ad44f94d
unknown <amsubhadeep@gmail.com>
2023-03-08 22:03:42 +0530

    Renamed project

---
+++ src/main/java/com/ecommerce/backendone/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.ecommerce.backendone.repository;
+
+import com.ecommerce.backendone.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE product_sequence SET next_val = 1", nativeQuery = true)
+    void resetSequence();
+}
---
+++ src/test/java/com/ecommerce/backendone/service/ProductServiceTest.java
@@ -0,0 +1,34 @@
+package com.ecommerce.backendone.service;
+
+import com.ecommerce.backendone.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    private ProductService underTest;
+    @Mock private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new ProductService(productRepository);
+    }
+
+    @Test
+    void getProductById_ShouldInvokeRepositoryFunction() {
+        // Given
+        Integer id = 1;
+
+        // When
+        underTest.getProductById(id);
+
+        // Then
+        verify(productRepository).findById(id);
+    }
+}
